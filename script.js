885b7a93cb6cd0785ec4ec568488edcb7cf84fd9
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-21 21:33:39 +1000

    created animation space and first shape

---
+++ script.js
@@ -0,0 +1,19 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
+
+var geometry = new THREE.SphereGeometry(0.5, 32, 32);
+var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
